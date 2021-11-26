f44cddc16d3cf1d1f499de2f440e24107953e987
omy-q <olya_aparina@mail.ru>
2021-11-26 15:43:42 +0300

    change activity, implements new method view interface, and add methods for init buttons and init buttons click listener

--- app/src/main/java/com/example/popularlibraries/MainActivity.kt
+++ app/src/main/java/com/example/popularlibraries/MainActivity.kt
@@ -2,36 +2,49 @@ package com.example.popularlibraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.example.popularlibraries.databinding.ActivityMainBinding
 import com.example.popularlibraries.presenter.MainPresenter
 import com.example.popularlibraries.view.MainView
 
 class MainActivity : AppCompatActivity(), MainView {
 
-    private var vb: ActivityMainBinding? = null
-    val presenter = MainPresenter(this)
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private val presenter = MainPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        vb = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(vb?.root)
-
-        val listener = View.OnClickListener {
-            presenter.counterClick(it.id)
-        }
-
-        vb?.btnCounter1?.setOnClickListener(listener)
-        vb?.btnCounter2?.setOnClickListener(listener)
-        vb?.btnCounter3?.setOnClickListener(listener)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+        initBtnListener()
     }
 
-    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
-    override fun setButtonText(index: Int, text: String) {
-        when(index){
-            0 -> vb?.btnCounter1?.text = text
-            1 -> vb?.btnCounter2?.text = text
-            2 -> vb?.btnCounter3?.text = text
+    private fun initView() {
+        presenter.initView()
+    }
+
+    private fun initBtnListener() {
+        binding.btnCounter1.setOnClickListener {
+            presenter.onFirstBtnClicked()
         }
+        binding.btnCounter2.setOnClickListener {
+            presenter.onSecondBtnClicked()
+        }
+        binding.btnCounter3.setOnClickListener {
+            presenter.onThirdBtnClicked()
+        }
+    }
+
+    override fun setFirstBtnText(text: String) {
+        binding.btnCounter1.text = text
+    }
+
+    override fun setSecondBtnText(text: String) {
+        binding.btnCounter2.text = text
+    }
+
+    override fun setThirdBtnText(text: String) {
+        binding.btnCounter3.text = text
     }
 }
