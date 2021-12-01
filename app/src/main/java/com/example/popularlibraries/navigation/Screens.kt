a6d51454e03cff01189c21246f88a6396c72adb6
omy-q <olya_aparina@mail.ru>
2021-12-01 22:34:03 +0300

    add screen interface and implementation

---
+++ app/src/main/java/com/example/popularlibraries/navigation/Screens.kt
@@ -0,0 +1,7 @@
+package com.example.popularlibraries.navigation
+
+import com.github.terrakok.cicerone.Screen
+
+interface Screens {
+    fun users() : Screen
+}
---
+++ app/src/main/java/com/example/popularlibraries/navigation/ScreensImplementation.kt
@@ -0,0 +1,9 @@
+package com.example.popularlibraries.navigation
+
+import com.github.terrakok.cicerone.Screen
+
+class ScreensImplementation: Screens{
+    override fun users(): Screen {
+        TODO("Not yet implemented")
+    }
+}
