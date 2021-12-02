229f82bfaad3e4af19f39c56ebde5b64d004a7e7
omy-q <olya_aparina@mail.ru>
2021-12-03 00:27:25 +0300

    add base view interface for viewHolder add view interface for user viewHolder create UserHolder class

---
+++ app/src/main/java/com/example/popularlibraries/ui/base/ItemView.kt
@@ -0,0 +1,5 @@
+package com.example.popularlibraries.ui.base
+
+interface ItemView {
+    var pos: Int
+}
---
+++ app/src/main/java/com/example/popularlibraries/ui/users/view/UserItemView.kt
@@ -0,0 +1,7 @@
+package com.example.popularlibraries.ui.users.view
+
+import com.example.popularlibraries.ui.base.ItemView
+
+interface UserItemView:ItemView {
+    fun setLogin(login:String)
+}
