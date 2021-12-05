8437ba08e30ee222fbfc3e072d4feee6e5dac13a
omy-q <olya_aparina@mail.ru>
2021-12-05 16:30:28 +0300

    create Observable source in Model and create Observer in Presenter

--- app/src/main/java/com/example/popularlibraries/ui/users/presenter/UsersPresenter.kt
+++ app/src/main/java/com/example/popularlibraries/ui/users/presenter/UsersPresenter.kt
@@ -1,10 +1,14 @@
 package com.example.popularlibraries.ui.users.presenter
 
+import android.util.Log
+import com.example.popularlibraries.data.User
 import com.example.popularlibraries.model.UsersModel
 import com.example.popularlibraries.navigation.Screens.userInfo
 import com.example.popularlibraries.ui.base.BasePresenter
 import com.example.popularlibraries.ui.users.view.UsersView
 import com.github.terrakok.cicerone.Router
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 class UsersPresenter(
     router: Router,
@@ -20,8 +24,28 @@ class UsersPresenter(
         }
     }
 
+    private val observer = object
+        : Observer<List<User>>{
+        override fun onSubscribe(d: Disposable) {
+            Log.d("RxJava", "Subscribed")
+        }
+
+        override fun onNext(users: List<User>) {
+            usersListPresenter.users.addAll(users)
+            viewState.updateUsers()
+        }
+
+        override fun onError(e: Throwable) {
+            Log.e("RxJava", e.stackTraceToString())
+        }
+
+        override fun onComplete() {
+            Log.d("RxJava", "Success")
+        }
+
+    }
+
     private fun loadData(){
-        usersListPresenter.users.addAll(model.getUsers())
-        viewState.updateUsers()
+        model.getUsers().subscribe(observer)
     }
 }
