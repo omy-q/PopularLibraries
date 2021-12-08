5eae6962f2d5334b701d08c775a2c3340acb7b15
omy-q <olya_aparina@mail.ru>
2021-12-08 18:57:28 +0300

    add method in model for get repositories from remote service

--- app/src/main/java/com/example/popularlibraries/model/UsersModel.kt
+++ app/src/main/java/com/example/popularlibraries/model/UsersModel.kt
@@ -1,9 +1,10 @@
 package com.example.popularlibraries.model
 
+import com.example.popularlibraries.data.Repository
 import com.example.popularlibraries.data.User
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 
 interface UsersModel {
     fun getUsers(): Single<List<User>>
+    fun getUserRepositories(url: String): Single<List<Repository>>
 }
--- app/src/main/java/com/example/popularlibraries/model/UsersModelImplementation.kt
+++ app/src/main/java/com/example/popularlibraries/model/UsersModelImplementation.kt
@@ -1,5 +1,6 @@
 package com.example.popularlibraries.model
 
+import com.example.popularlibraries.data.Repository
 import com.example.popularlibraries.data.User
 import com.example.popularlibraries.remote.RetrofitService
 import io.reactivex.rxjava3.core.Single
@@ -10,5 +11,9 @@ class UsersModelImplementation(
 
     override fun getUsers(): Single<List<User>> {
         return remoteService.getUsers()
+    }
+
+    override fun getUserRepositories(url: String): Single<List<Repository>> {
+        return remoteService.getRepositories(url)
     }
 }
