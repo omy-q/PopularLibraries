7d17f1661032cbf675c41ceb379225ebd769e347
omy-q <olya_aparina@mail.ru>
2021-12-08 18:58:34 +0300

    change Presenter, Fragment and View interface for show repositories on screen

--- app/src/main/java/com/example/popularlibraries/ui/user_info/presenter/UserInfoPresenter.kt
+++ app/src/main/java/com/example/popularlibraries/ui/user_info/presenter/UserInfoPresenter.kt
@@ -1,11 +1,38 @@
 package com.example.popularlibraries.ui.user_info.presenter
 
+import android.util.Log
+import com.example.popularlibraries.data.Repository
+import com.example.popularlibraries.data.User
+import com.example.popularlibraries.model.UsersModel
 import com.example.popularlibraries.ui.base.BasePresenter
 import com.example.popularlibraries.ui.user_info.view.UserInfoView
 import com.github.terrakok.cicerone.Router
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class UserInfoPresenter(
-    router: Router
+    router: Router,
+    private val model: UsersModel
 ) : BasePresenter<UserInfoView>(router) {
+
+    fun getData(user: User){
+        viewState.setData(user)
+        loadData(user.reposUrl)
+    }
+
+    private fun loadData(url: String) {
+        model.getUserRepositories(url)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({reps ->
+                       viewState.updateRepositories(reps)
+            }, {
+                Log.d("reps", "Error: ${it.stackTrace}")
+            })
+    }
+
+    fun onItemClicked(repo: Repository){
+        Log.d("reps", "repo clicked: ${repo}")
+    }
 
 }
--- app/src/main/java/com/example/popularlibraries/ui/user_info/view/UserInfoFragment.kt
+++ app/src/main/java/com/example/popularlibraries/ui/user_info/view/UserInfoFragment.kt
@@ -2,36 +2,55 @@ package com.example.popularlibraries.ui.user_info.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.example.popularlibraries.App
 import com.example.popularlibraries.R
+import com.example.popularlibraries.data.Repository
 import com.example.popularlibraries.data.User
 import com.example.popularlibraries.databinding.FragmentUserInfoBinding
+import com.example.popularlibraries.model.UsersModelImplementation
+import com.example.popularlibraries.remote.ApiHolder
 import com.example.popularlibraries.ui.base.BaseFragment
 import com.example.popularlibraries.ui.user_info.presenter.UserInfoPresenter
+import com.example.popularlibraries.ui.user_info.view.recyclerview.RepositoryAdapter
 import moxy.ktx.moxyPresenter
 
 class UserInfoFragment : BaseFragment<FragmentUserInfoBinding>(FragmentUserInfoBinding::inflate),
     UserInfoView {
 
     private val presenter by moxyPresenter {
-        UserInfoPresenter(App.instance.router)
+        UserInfoPresenter(App.instance.router, UsersModelImplementation(ApiHolder.retrofitService))
+    }
+
+    private val adapter by lazy {
+        RepositoryAdapter(presenter::onItemClicked)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val user = arguments?.let { it.getParcelable<User>(USER_ID)} as? User
-        user?.let { initView(it) }
+        val user = arguments?.let { it.getParcelable<User>(USER_ID)}
+        user?.let { presenter.getData(it) }
+        initView()
     }
 
-    private fun initView(user : User) {
+    private fun initView() {
+        binding.userRepositories.layoutManager = GridLayoutManager(context, 3)
+        binding.userRepositories.adapter = adapter
+    }
+
+    override fun setData(user: User) {
         binding.userInfoAvatar.load(user.avatarUrl){
+            transformations(CircleCropTransformation())
             placeholder(R.drawable.progress_animation)
             error(R.drawable.error_image_load)
         }
         binding.userInfoLogin.text = user.login
-        binding.userInfoHtml.text = user.htmlUrl
+    }
+
+    override fun updateRepositories(reps: List<Repository>) {
+        adapter.submitList(reps)
     }
 
     override fun backPressed() = presenter.backPressed()
