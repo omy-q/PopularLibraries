5a7d35e205ff31c907db8f47a2c82771651455c5
omy-q <olya_aparina@mail.ru>
2021-12-08 12:57:39 +0300

    delete unnecessary classes

--- app/src/main/java/com/example/popularlibraries/ui/base/recyclerview/ItemView.kt
+++
@@ -1,5 +0,0 @@
-package com.example.popularlibraries.ui.base.recyclerview
-
-interface ItemView {
-    var pos: Int
-}
--- app/src/main/java/com/example/popularlibraries/ui/users/view/recyclerview/UserItemView.kt
+++
@@ -1,7 +0,0 @@
-package com.example.popularlibraries.ui.users.view.recyclerview
-
-import com.example.popularlibraries.ui.base.recyclerview.ItemView
-
-interface UserItemView: ItemView {
-    fun setLogin(login:String)
-}
