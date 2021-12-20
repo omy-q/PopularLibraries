acc020c8483492b888e86531e8ac8b466e1cca1b
omy-q <olya_aparina@mail.ru>
2021-12-20 19:42:02 +0300

    create repositories module

---
+++ app/src/main/java/com/example/popularlibraries/di/modules/RepositoryModule.kt
@@ -0,0 +1,31 @@
+package com.example.popularlibraries.di.modules
+
+import com.example.popularlibraries.model.ReposModel
+import com.example.popularlibraries.model.ReposModelImplementation
+import com.example.popularlibraries.model.UsersModel
+import com.example.popularlibraries.model.UsersModelImplementation
+import com.example.popularlibraries.remote.RetrofitService
+import com.example.popularlibraries.remote.connectivity.NetworkStatus
+import com.example.popularlibraries.room.model.RoomRepoModel
+import com.example.popularlibraries.room.model.RoomUserModel
+import dagger.Provides
+
+class RepositoryModule {
+    @Provides
+    fun usersModel(
+        status: NetworkStatus,
+        remoteService: RetrofitService,
+        roomModel: RoomUserModel
+    ):UsersModel{
+        return UsersModelImplementation(status, remoteService, roomModel)
+    }
+
+    @Provides
+    fun reposModel(
+        status: NetworkStatus,
+        remoteService: RetrofitService,
+        roomModel: RoomRepoModel
+    ): ReposModel{
+        return ReposModelImplementation(status, remoteService, roomModel)
+    }
+}
