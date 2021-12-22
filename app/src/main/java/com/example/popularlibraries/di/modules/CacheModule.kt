b7c48e3c364e8ed5358d9b4b95d2e65eb8857d71
omy-q <olya_aparina@mail.ru>
2021-12-22 18:03:07 +0300

    add room models dependencies in dagger2

--- app/src/main/java/com/example/popularlibraries/di/modules/CacheModule.kt
+++ app/src/main/java/com/example/popularlibraries/di/modules/CacheModule.kt
@@ -3,6 +3,10 @@ package com.example.popularlibraries.di.modules
 import android.content.Context
 import androidx.room.Room
 import com.example.popularlibraries.room.DataBase
+import com.example.popularlibraries.room.model.RoomRepoModel
+import com.example.popularlibraries.room.model.RoomRepoModelImplementation
+import com.example.popularlibraries.room.model.RoomUserModel
+import com.example.popularlibraries.room.model.RoomUserModelImplementation
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -18,4 +22,16 @@ class CacheModule {
         Room
             .databaseBuilder(context, DataBase::class.java, DB_NAME)
             .build()
+
+    @Singleton
+    @Provides
+    fun roomRepoModel(db: DataBase): RoomRepoModel{
+        return RoomRepoModelImplementation(db)
+    }
+
+    @Singleton
+    @Provides
+    fun roomUserModel(db: DataBase): RoomUserModel{
+        return RoomUserModelImplementation(db)
+    }
 }
